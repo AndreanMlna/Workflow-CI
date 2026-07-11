3cccbaccdca6d17f71b7cc67c26400a4e9e48ca2
andrian maulana <134795751+AndreanMlna@users.noreply.github.com>
2026-07-11 15:22:00 +0700

    Initial commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import mlflow
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# Mengaktifkan pencatatan otomatis
+mlflow.autolog()
+
+def train_model():
+    print("Memuat dataset preprocessing...")
+    df = pd.read_csv('ecommerce_data_preprocessing.csv')
+    X = df.drop(columns=['Is_Churn'])
+    y = df['Is_Churn']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    with mlflow.start_run() as run:
+        print("Melatih model RandomForest...")
+        rf = RandomForestClassifier(n_estimators=100, random_state=42)
+        rf.fit(X_train, y_train)
+
+        # Trik Advance: Simpan Run ID ke file teks untuk dibaca oleh GitHub Actions
+        run_id = run.info.run_id
+        with open("run_id.txt", "w") as f:
+            f.write(run_id)
+
+        print(f"Pelatihan selesai! Run ID: {run_id}")
+
+if __name__ == "__main__":
+    train_model()
